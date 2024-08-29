d0168bb27756914c79bffdf5da46a391d24340ba
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-08-29 15:59:25 +0100

    done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.layout')
+@section('title')
+     Welcome to  {{ config('app.name') }}
+@endsection
 @section('content')
     <img src="/img/logo.png" alt="{{ config('app.name') }} logo">
     <p> {{ session('message') }}</p>
     <a href="{{ route('pizzas.create') }}">Order a pizza</a>
-
-
 @endsection
