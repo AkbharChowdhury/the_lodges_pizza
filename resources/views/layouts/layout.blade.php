d51c649945bf025015237040daa8c5c0069fb65c
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-08-30 12:19:57 +0100

    working

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -10,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="/css/style.css">
+
+{{--    @vite(['resources/css/app.scss', 'resources/js/app.js'])--}}
 </head>
 <body class="font-sans antialiased">
     @yield('content')
